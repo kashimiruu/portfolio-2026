ccb4524b8b687222639cab4418b64ef6fb120b49
Jovan Kashmir <68640338+kashimiruu@users.noreply.github.com>
2025-12-13 15:22:49 -0600

    Add files via upload

--- js/assets.js
+++ js/assets.js
@@ -2,10 +2,10 @@ import Assets from "./class/assets.js";
 
 (async () => {
     window.assets = new Assets();
-    await assets.set("ocean_eyes", "../vid/ocean-eyes_yt.mp4");
-    await assets.set("design_thinking", "../vid/design-thinking_yt.mp4");
-    await assets.set("intro", "../vid/intro_yt.mp4");
-    await assets.set("tamframe", "../vid/tamframe_yt.mp4");
-    await assets.set("ticap", "../vid/ticap_yt.mp4");
+    await assets.set("ocean_eyes", "./vid/ocean-eyes_yt.mp4");
+    await assets.set("design_thinking", "./vid/design-thinking_yt.mp4");
+    await assets.set("intro", "./vid/intro_yt.mp4");
+    await assets.set("tamframe", "./vid/tamframe_yt.mp4");
+    await assets.set("ticap", "./vid/ticap_yt.mp4");
     window.assetsLoaded = true;
 })();
